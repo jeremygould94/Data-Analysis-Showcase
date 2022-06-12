4a8ba8203af92ad7f5cb54dd61cb87e1799ebd76
jeremygould94 <105975469+jeremygould94@users.noreply.github.com>
2022-06-12 11:26:50 +0100

    Add files via upload

---
+++ lazypredict_test.py
@@ -0,0 +1,37 @@
+# ----- LazyPredict Classification Example -----
+
+from lazypredict.Supervised import LazyClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+data = load_iris()
+X = data.data
+y = data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=123)
+
+clf = LazyClassifier(verbose=0, ignore_warnings=True, custom_metric=None)
+models, predictions = clf.fit(X_train, X_test, y_train, y_test)
+
+print(models)
+
+# ----- LazyPredict Regression Example -----
+
+from lazypredict.Supervised import LazyRegressor
+from sklearn import datasets
+from sklearn.utils import shuffle
+import numpy as np
+
+boston = datasets.load_boston()
+X, y = shuffle(boston.data, boston.target, random_state=13)
+X = X.astype(np.float32)
+
+offset = int(X.shape[0] * 0.9)
+
+X_train, y_train = X[:offset], y[:offset]
+X_test, y_test = X[offset:], y[offset:]
+
+reg = LazyRegressor(verbose=0, ignore_warnings=False, custom_metric=None)
+models, predictions = reg.fit(X_train, X_test, y_train, y_test)
+
+print(models)
